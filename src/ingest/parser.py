e1309565d8015d0edd53b1b29dc4313df2614310
grct <guerciamattiaa@gmail.com>
2025-01-22 01:48:49 +0100

    pulizia codice

--- src/ingest/parser.py
+++ src/ingest/parser.py
@@ -1,24 +1,9 @@
-import os
-from pprint import pprint
-
-from llama_parse import LlamaParse
-
-parser = LlamaParse(
-    api_key=os.environ["LLAMA"],  # can also be set in your env as LLAMA_CLOUD_API_KEY
-    num_workers=9,  # if multiple files passed, split in `num_workers` API calls
-    verbose=True,
-    result_type="markdown",
-    language="it",
-    fast_mode=False
-)
-
-# a = parser.get_json_result("\Cartelle/Dev/Python/GraphRAG/docs/Menu/Anima Cosmica.pdf")
-
 from llama_index.core import SimpleDirectoryReader
+from src.settings import parser, parsing_path
 
 file_extractor = {".pdf": parser}
 documents = SimpleDirectoryReader(
-    "\Cartelle/Dev/Python/GraphRAG/docs/Menu", file_extractor=file_extractor
+    parsing_path, file_extractor=file_extractor
 )
 
 i = 0
--- src/settings.py
+++ src/settings.py
@@ -3,10 +3,21 @@ import os
 from langchain_ibm import ChatWatsonx
 from langchain_openai import ChatOpenAI
 from langchain_neo4j import Neo4jGraph
+from llama_parse import LlamaParse
 
 llm_small = ChatOpenAI(model="gpt-4o-mini")
-
 llm = ChatOpenAI(model="gpt-4o")
 
+parser = LlamaParse(
+    api_key=os.environ["LLAMA"],  # can also be set in your env as LLAMA_CLOUD_API_KEY
+    num_workers=9,  # if multiple files passed, split in `num_workers` API calls
+    verbose=True,
+    result_type="markdown",
+    language="it",
+    fast_mode=False
+)
+parsing_path = "\Cartelle/Dev/Python/GraphRAG/docs/Menu"
+
 graph = Neo4jGraph()
+
 
